ffb14b97deb77404a1d98d2920ad320deba1dae7
Danat Nurmambetov <danat-1998@mail.ru>
2025-08-17 02:37:44 +0300

    Add layout.html

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,9 +1,12 @@
+import psycopg2
 import os
 
 from dotenv import load_dotenv
 from flask import Flask, render_template
 
 load_dotenv()
+DATABASE_URL = os.getenv('DATABASE_URL')
+conn = psycopg2.connect(DATABASE_URL)
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
